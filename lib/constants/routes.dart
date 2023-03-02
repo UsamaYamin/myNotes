f32aecdaf1e8f2762abf62c56032d08b50067577
Usama Ch <uch43062@gmail.com>
2023-03-02 09:27:44 +0500

    step 1

---
+++ lib/constants/routes.dart
@@ -0,0 +1,3 @@
+const loginRout = '/login/';
+const registerRout = '/register/';
+const notesRout = '/notes/';
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firstapp/constants/routes.dart';
+import 'package:firstapp/firebase_options.dart';
+import 'package:firstapp/views/login_views.dart';
+import 'package:firstapp/views/notes_view.dart';
+import 'package:firstapp/views/register_view.dart';
+import 'package:firstapp/views/verify_email.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      routes: {
+        loginRout: (context) => const LoginView(),
+        registerRout: (context) => const RegisterView(),
+        notesRout: (context) => const RegisterView(),
+      },
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: ((context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                devtools.log('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const NotesView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      }),
+    );
+  }
+}
